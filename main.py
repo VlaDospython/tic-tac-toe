f9c9e0e00a32307bd1eac46343f0e0ce7d080717
chabaniuk-m <chabanyuk.maks@gmail.com>
2024-01-03 11:01:57 +0200

    abstract implementation

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+import random
+import sys
 
 
 
 
 if __name__ == '__main__':
-    pass
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    player = random.choice('XY')                # get a random player
+
+    # game loop
+    while not is_draw(board):
+        # TODO: change player
+        print_board(board)
+        put_mark_on_board(board, player)
+        if is_win(board, player):
+            # TODO: greet a player with a win
+            break
+    else:
+        # tell that it is a draw
+        print("Draw :[")
+
+    print("Thank you for playing")
+
 
